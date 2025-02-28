861b5b8a8b0742e0d95612037f7c85e19aff9adb
Rock junior seide <seiderock@yahoo.com>
2025-02-28 16:00:03 -0500

    did the bird class

---
+++ Zoo.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Zoo here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Zoo
+{
+    private ArrayList<Bird> zooBirds;
+    
+    
+public Zoo(){
+    zooBirds = new Arraylist<>();
+    zooBirds.add(new Bird("blue","blue jay",2));
+    zooBirds.add(new Bird("red","Blue jay",2));
+    zooBirds.add(new Bird("blue","blue jay",2));
+    zooBirds.add(new Bird("red","Blue jay",2));
+    zooBirds.add(new Bird("blue","blue jay",2));
+}
+    
+ int count0   
+  
