30c334de8efe925318d0e036d9bdfeca5e9a4c01
Rock junior seide <seiderock@yahoo.com>
2025-02-28 15:01:38 -0500

    added salary class

---
+++ Salary.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Salary here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Salary
+{
+    
+    /*Question 14
+     * worked hours = worked hours  
+     * hourly wage = hourly wage
+     * total salary = worked hours X hourly wage
+     * taxes/deductions = 30% of total salary
+     * net pay = total salary - taxes/deductions
+     */
+}
